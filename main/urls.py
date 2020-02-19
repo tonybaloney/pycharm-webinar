9815d3e0e3b008ca4842a554584622b60ba1b390
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-02-19 21:22:06 +1100

    simple layouts

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', home),
 ]
--- webinar/settings.py
+++ webinar/settings.py
@@ -44,7 +44,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -58,6 +57,19 @@ TEMPLATES = [
         ,
         'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
